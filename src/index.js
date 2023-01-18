ba9b65024f8592bcdfcf7d5e3599b76d8e884077
David <davidmejia.05@hotmail.com>
2023-01-18 17:16:40 -0600

    index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,107 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Button } from "./Button";
+//import { Greetings, Greetings2, UserCard } from "./Greetings";
+//import Product, { Navbar } from "./Product";
+//import { TaskCard } from "./Task";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//import { Saludar } from "./Saludar";
+import { Posts } from "./Posts";
+import { useState } from "react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const handleChange = (e) => {
+  console.log(e.target.value);
+};
+
+/* const users = [
+  {id: 1,
+  name: 'david',
+image: 'https://robohash.org/david'
+},
+{id: 2,
+  name: 'pxko',
+image: 'https://robohash.org/pxko'
+},
+{id: 3,
+  name: 'xris',
+image: 'https://robohash.org/xris'
+}, 
+]*/
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <>
+      <h1>Counter {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter+1);
+        }}
+      >
+        Sumar
+      </button>
+      <button
+        onClick={() => {
+          setCounter(counter-1);
+        }}
+      >
+        Restar
+      </button>
+    </>
+  );
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <>
+    {/*     <UserCard
+      name="dmg"
+      amount={30}
+      married={true}
+      points={[1, 2, 3]}
+      address={{ calle: "ksk", n: "112" }}
+      greet = {function (){ alert('hey')}}
+    />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+<UserCard
+      name="leono"
+      amount={30}
+      married={true}
+      points={[1, 2, 3]}
+      address={{ calle: "ksk", n: "112" }}
+      greet = {function (){ alert('hey')}}
+    /> */}
+
+    {/*   <Button text="pagar"/>
+  <Button text="regresar"/>
+  <Button text="bye" name="david" />
+ */}
+
+    {/*  <TaskCard ready={false}/>
+ <Saludar/> */}
+
+    {/* <Button text="pagar"/>
+<input id="HOLA" onChange={handleChange}></input>
+
+<form onSubmit={(e)=> {
+    e.preventDefault()
+    console.log('enviando datos')
+}
+}>
+  <h1>Registro de usuario</h1>
+
+  <button>hola</button>
+</form>
+<Posts/> */}
+
+    {/* {users.map((user, i)=> {
+  return <div key={i}>
+    <h1>{user.name}</h1>
+    <img src={user.image}></img>
+  </div>
+})} */}
+
+    <Counter />
+  </>
+);
