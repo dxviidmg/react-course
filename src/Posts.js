803b07c76a2847622330194ac053a09117ea5a40
David <davidmejia.05@hotmail.com>
2023-01-18 17:16:23 -0600

    posts

---
+++ src/Posts.js
@@ -0,0 +1,13 @@
+import { BsFillAlarmFill } from "react-icons/bs";
+
+
+export const Posts = () => {
+    return <button onClick={()=>{
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    }}>
+        <BsFillAlarmFill/>
+        Traer datos</button>
+}
